7d84ca264033e72ef81aab90a5ceffa856172be2
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-01-26 10:22:10 -0500

    se agrega comentario

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  // eslint-disable-next-line @typescript-eslint/no-un
   await app.listen(3000);
 }
 bootstrap();
