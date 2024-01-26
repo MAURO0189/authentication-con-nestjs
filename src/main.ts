924023a0ceb390a8425228e34732184a4be48af9
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-01-26 11:58:11 -0500

    mian change

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // eslint-disable-next-line @typescript-eslint/no-un
-  //const port = process.env.PORT || 3001;
   await app.listen(3000);
 }
 bootstrap();
