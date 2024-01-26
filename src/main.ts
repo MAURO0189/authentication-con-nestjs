5f6e22fa43f75c4b4ef74d5de94332f432344a2c
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-01-26 12:18:23 -0500

    coments

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // eslint-disable-next-line @typescript-eslint/no-un
+  //
   await app.listen(3000);
 }
 bootstrap();
