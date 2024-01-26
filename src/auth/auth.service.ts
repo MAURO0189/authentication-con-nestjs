d6809b346598812c1b7444928d6e1bd9f06943e0
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-01-26 10:44:31 -0500

    Primer commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,53 @@
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { LoginDto } from 'src/auth/dto/login.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcryptjs from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from './dto/register.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register({ name, email, password }: RegisterDto) {
+    const user = await this.usersService.findOneByEmail(email);
+
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+
+    return await this.usersService.create({
+      name,
+      email,
+      password: await bcryptjs.hash(password, 10),
+    });
+  }
+
+  async login({ email, password }: LoginDto) {
+    const user = await this.usersService.findOneByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('Email no encontrado');
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Contraseña incorrecta');
+    }
+
+    const payload = { email: user.email };
+
+    const token = await this.jwtService.signAsync(payload);
+
+    return {
+      token,
+      email,
+    };
+  }
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  Index,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint', unsigned: true })
+  id: number;
+
+  @Index({ unique: true })
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Index({ unique: true })
+  @Column({ unique: true, nullable: false })
+  password: string;
+
+  @Index()
+  @Column({ nullable: true })
+  uuid: string | null;
+
+  @CreateDateColumn({
+    precision: 0,
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    precision: 0,
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  lastUpdate: Date;
+  firebaseUuid: string;
+}
