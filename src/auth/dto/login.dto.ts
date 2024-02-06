0b79b54a19ec00270dfd94da6043fd2da2353b7d
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-02-06 09:18:37 -0500

    se quita nombre de usuario

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,10 +2,6 @@ import { Transform } from 'class-transformer';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @IsString()
-  @MinLength(3)
-  name: string;
-
   @IsEmail()
   email: string;
 
